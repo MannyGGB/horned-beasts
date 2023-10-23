fdb9680dcfc7a64189aa3f670a6ff0b16ee79203
Manuel Gonzalez Garcia de Blas <manugonzalez.tech@gmail.com>
2023-10-23 21:29:54 +0100

    Render two animals

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,26 +1,43 @@
-// import animal from "./assets/animal.jpg";
+import HornedBeast from "./HornedBeast";
 
 export default function Gallery(props) {
-  class Beast {
-    constructor(title, imageUrl, description) {
-      this.title = title;
-      this.imageUrl = imageUrl;
-      this.description = description;
-    }
-  }
-
-  return (badger = new Beast("badger", "./assets/animal.jpg", "description"));
-  // <section>
-  //   <div className="beast">
-  //     <h2 className="title">Animal Name</h2>
-  //     <img className="imageUrl" src="" alt="animal" title="animal" />
-  //     <p className="description">description</p>
-  //   </div>
-
-  //   <div className="beast">
-  //     <h2 className="title">Animal Name</h2>
-  //     <img className="imageUrl" src="" alt="animal" title="animal" />
-  //     <p className="description">description</p>
-  //   </div>
-  // </section>
+  return (
+    <section>
+      <div>
+        <HornedBeast title="animal" />
+        <HornedBeast imageUrl="https://www.fundacionaquae.org/wp-content/uploads/2018/10/proteger-a-los-animales-1024x654.jpg" />
+        <HornedBeast description="animal info" />
+      </div>
+      <div>
+        <HornedBeast title="animal" />
+        <HornedBeast imageUrl="https://www.fundacionaquae.org/wp-content/uploads/2018/10/proteger-a-los-animales-1024x654.jpg" />
+        <HornedBeast description="animal info" />
+      </div>
+    </section>
+  );
 }
+
+// export default function Gallery(props) {
+//   class Beast {
+//     constructor(title, imageUrl, description) {
+//       this.title = title;
+//       this.imageUrl = imageUrl;
+//       this.description = description;
+//     }
+//   }
+
+//   return (badger = new Beast("badger", "./assets/animal.jpg", "description"));
+//   <section>
+//     <div className="beast">
+//       <h2 className="title">Animal Name</h2>
+//       <img className="imageUrl" src="" alt="animal" title="animal" />
+//       <p className="description">description</p>
+//     </div>
+
+//     <div className="beast">
+//       <h2 className="title">Animal Name</h2>
+//       <img className="imageUrl" src="" alt="animal" title="animal" />
+//       <p className="description">description</p>
+//     </div>
+//   </section>
+// }
--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,12 +1,10 @@
-import Gallery from "./Gallery";
-
 export default function HornedBeast(props) {
   return (
     <section>
       <div>
-        <h2>{title}</h2>
-        <img src={imageUrl} alt="animal" title="animal" />
-        <p>{description}</p>
+        <h2>{props.title}</h2>
+        <img src={props.imageUrl} alt="animal" title="animal" />
+        <p>{props.description}</p>
       </div>
     </section>
   );
