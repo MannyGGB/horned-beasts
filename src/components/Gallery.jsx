acbc3399c7a2f32b355ad80d55bdf643dd527268
Manuel Gonzalez Garcia de Blas <manugonzalez.tech@gmail.com>
2023-10-24 15:03:39 +0100

    Add data.json and state to count likes

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,26 +1,51 @@
+//import animal from ""../assets/animal.jpg" LAB 01
+
 import HornedBeast from "./HornedBeast";
+import data from "../data.json"; //when we import, we can use data directly without declaring a variable
+
+//every function needs a return; in this case, we have two returns
 
 export default function Gallery() {
   return (
+    // you can't declare variables inside the return
     <section className="animal-section">
-      <div>
-        <HornedBeast
-          title="animal"
-          imageUrl="https://www.fundacionaquae.org/wp-content/uploads/2018/10/proteger-a-los-animales-1024x654.jpg"
-          description="animal info"
-        />
-      </div>
-      <div>
-        <HornedBeast
-          title="animal"
-          imageUrl="https://www.fundacionaquae.org/wp-content/uploads/2018/10/proteger-a-los-animales-1024x654.jpg"
-          description="animal info"
-        />
-      </div>
+      {data.map(function (beast) {
+        // the .map method takes two parameters max (one is the array item, the other is the index)
+        return (
+          <HornedBeast
+            title={beast.title} // we can access the beast properties with dot notation
+            imageUrl={beast.image_url}
+            description={beast.description}
+          />
+        );
+      })}
     </section>
   );
 }
 
+// LAB 01
+// export default function Gallery() {
+//   return (
+//     <section className="animal-section">
+//       <div>
+//         <HornedBeast
+//           title="animal"
+//           imageUrl={animal}
+//           description="animal info"
+//         />
+//       </div>
+//       <div>
+//         <HornedBeast
+//           title="animal"
+//           imageUrl={animal}
+//           description="animal info"
+//         />
+//       </div>
+//     </section>
+//   );
+// }
+
+// EXTRA CODE (was trying to use a class and constructor)
 // export default function Gallery(props) {
 //   class Beast {
 //     constructor(title, imageUrl, description) {
--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,10 +1,17 @@
-export default function HornedBeast(props) {
+import { useState } from "react";
+
+export default function HornedBeast({ title, imageUrl, description }) {
+  const [likes, setLikes] = useState(0);
+  function handleLikes() {
+    setLikes(likes + 1);
+  }
   return (
     <section>
       <div>
-        <h2>{props.title}</h2>
-        <img src={props.imageUrl} alt="animal" title="animal" />
-        <p>{props.description}</p>
+        <h2>{title}</h2>
+        <img src={imageUrl} alt="animal" title="animal" onClick={handleLikes} />
+        <p>{description}</p>
+        <p>😍 {likes}</p>
       </div>
     </section>
   );
